cede94e8d8026e93158849aef4c03ecd88026810
Roxana1981 <roxanaameijide81@gmail.com>
2023-09-13 22:16:41 +0000

    Add consts and evenlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+const choices = document.querySelectorAll(".choices");
+let playerScore = 0;
+let computerScore = 0;
+
+choices.forEach(choice) => {
+    choice.addEventListener("click", function() {
+        const playerInput = this.name;
+
+        const computerChoices (playerInput, computerInput);
+        compareInputs(playerInput, computerInput);
+        updateScore();
+        if(checkWinner()) {
+            playerScore = computerScore = 0;
+            updateScore();
+        }
+      });
+    };
+
+
+
 function updateSelections(playerInput, computerInput){
 
 }
