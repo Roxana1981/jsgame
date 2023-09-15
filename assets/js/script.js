41a28789281072686693cfe1c4e242af45e2f847
Roxana1981 <roxanaameijide81@gmail.com>
2023-09-15 20:40:55 +0000

    Update compare inputs and update selection functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,83 @@
+// Consts and variables
+
 const choices = document.querySelectorAll(".choices");
 let playerScore = 0;
 let computerScore = 0;
 
-choices.forEach(choice) => {
-    choice.addEventListener("click", function() {
+// Eventlistener for selection of choices
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", function () {
         const playerInput = this.name;
 
         const computerChoices = ["rock", "paper", "scissors"];
         const computerInput = computerChoices[Math.floor(Math.random() * 3)];
 
+        updateSelections(playerInput, computerInput);
         compareInputs(playerInput, computerInput);
         updateScore();
-        if(checkWinner()) {
-            playerScore = computerScore = 0;
+        if(checkWinner()){
+            playerScore = computerScore= 0;
             updateScore();
         }
       });
-    };
+    });
 
 
 
 function updateSelections(playerInput, computerInput){
-
+    document.getElementById("player-selection").src = `./assets/images/${playerInput}.png`;
+    document.getElementById("computer-selection").src =`./assets/images/${computerInput}.png`;
 }
 
 function compareInputs(playerInput, computerInput){
+    const currentMatch = `${playerInput} vs ${computerInput}`;
+    if (playerInput === computerInput) {
+        document.getElementById("result").textContent = `${currentMatch} is a Tie`;
+        document.getElementById("result").style.color = "black";
+        return;
+    }
+// logic for rock selection
+
+    if (playerInput === "rock") {
+        if (computerInput === "scissors") {
+            document.getElementById("result").textContent = `${currentMatch} You win!`;
+            document.getElementById("result").style.color.bold = "green";
+            playerScore++;
+        }   else {
+            document.getElementById("result").textContent = `${currentMatch} Computer wins`;
+            document.getElementById("result").style.color = "red";
+            computerScore++;
+        } 
+      }
+// logic for paper selection
+
+    else if (playerInput === "paper") {
+        if (computerInput === "rock") {
+            document.getElementById("result").textContent = `${currentMatch} You win!`;
+            document.getElementById("result").style.color = "green";
+            playerScore++;
+        }   else { 
+            document.getElementById("result").textContent = `${currentMatch} Computer wins`;
+            document.getElementById("result").style.color = "red";
+            computerScore++;
+        }
+    }
+
+//logic for scissors selection
+
+    else {
+        if (computerInput === "paper") {
+            document.getElementById("result").textContent = `${currentMatch} You win`;
+            document.getElementById("result").style.color = "green";
+            playerScore++;
+        }   else {
+            document.getElementById("result").textContent = `${currentMatch} Computer wins`;
+            document.getElementById("result").style.color = "red";
+            computerScore++;
+            
+        }
+    }
 
 }
 
@@ -46,4 +99,3 @@ function checkWinner () {
     return false;
 }
 
-}
